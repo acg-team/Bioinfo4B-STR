6df1329c45b6f9b7f7074e5a6cf9e67f77fc339c
maverbiest <mavh1995@gmail.com>
2023-05-09 17:45:11 +0200

    Further developed STR project. Also added genome annotation in GTF format for APC gene

---
+++ scripts/bash_scripts/02_bwa-mem2_alignment.sh
@@ -0,0 +1,8 @@
+#!/bin/env/bash
+set -euo pipefail
+
+# Perform the alignment for the reads against the reference sequence
+bwa-mem2 mem \
+-R '@RG\tID:sim20230508\tPL:wgsim\tSM:mut' \
+-t 4 ../../data/reference/APC.fa ../../data/reads/APC_mut_out1.fq.gz ../../data/reads/APC_mut_out2.fq.gz \
+> ../../data/alignments/APC_mut.sam
